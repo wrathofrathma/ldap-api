e20b73220f81885c14af15475e02eed844c21c08
Rathma <rathma@kyso.dev>
2022-03-01 09:37:11 -0500

    Cleaning up some example code

--- app.ts
+++ app.ts
@@ -14,12 +14,6 @@ import indexRouter from './app/routes/index';
 /** Configure application to use routers **/
 app.use('/', indexRouter);
 
-/** Global Middleware Example */
-import ExampleMiddleware from './app/http/middleware/ExampleMiddleware'
-
-/** Using the middleware on all routes */
-app.use(ExampleMiddleware);
-
 /** Using a custom error handler */
 import ExceptionHandler from './app/exceptions/ExceptionHandler';
 app.use(ExceptionHandler);
